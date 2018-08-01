9b51cc620137c5045dad92239d376d3f69a0661b
mnao305 <naomasa305@gmail.com>
2018-08-02 06:35:38 +0900

    add:FizzBuzzするやつ

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	path = path[10:]
+	n, e := strconv.Atoi(path)
+	if e != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("invalid"))
+		return
+	}
+	fmt.Fprint(w, fizubuzz(n))
+}
+
+func fizubuzz(n int) string {
+	switch {
+	case n%15 == 0:
+		return "FizzBuzz"
+	case n%3 == 0:
+		return "Fizz"
+	case n%5 == 0:
+		return "Buzz"
+	default:
+		return strconv.Itoa(n)
+	}
+}
+
+func main() {
+	http.HandleFunc("/fizzbuzz/", handler)
+	http.ListenAndServe(":8080", nil)
+}
